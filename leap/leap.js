5efe27caf6fcd4839a8a7bc9748449a6c91a65f7
Thomas Huisking <tomasfaustin@gmail.com>
2017-07-11 20:28:50 -0700

    Update leap and testing suite to complete problem

--- leap/leap.js
+++ leap/leap.js
@@ -1,25 +1,17 @@
 //
 // This is only a SKELETON file for the "Leap" exercise. It's been provided as a
 // convenience to get you started writing code faster.
-//
-// ```plain
-// on every year that is evenly divisible by 4
-//   except every year that is evenly divisible by 100
-//     unless the year is also evenly divisible by 400
-// ```
-//
-// For example, 1997 is not a leap year, but 1996 is.  1900 is not a leap
-// year, but 2000 is.
 
 var Year = function(year) {
   this.year = year;
 };
 
 Year.prototype.isLeap = function() {
-  if (this.year % 4 !== 0) {
-    return false
+  if (this.year % 4 === 0 && this.year % 100 !== 0 || this.year % 400 === 0) {
+    return true;
+  } else {
+    return false;
   }
-
 };
 
 module.exports = Year;
