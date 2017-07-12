b141e07fa5da9044485981a65d9a9fdd2ad19259
Thomas Huisking <tomasfaustin@gmail.com>
2017-07-12 16:34:47 -0700

    Update hamming exercise to pass all but one test

--- hamming/hamming.js
+++ hamming/hamming.js
@@ -1,9 +1,15 @@
-
 var Hamming = function () {};
   Hamming.prototype.compute = function(a, b) {
-  if (a.length === b.length) {
-    return 0;
-  }
+    var count = 0;
+    if (a.length !== b.length) {
+      return 'Error: DNA strands must be of equal length.'
+    }
+      for (var i = 0; i <= a.length; i++) {
+        if (a.charAt(i) !== b.charAt(i)) {
+          count += 1;
+        }
+    }
+    return count;
 };
 
 module.exports = Hamming;
