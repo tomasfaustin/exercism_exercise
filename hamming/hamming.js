7f473a6cffcd1c0253427af3153107846375f789
Thomas Huisking <tomasfaustin@gmail.com>
2017-07-12 16:38:46 -0700

    Update hamming to pass all tests

--- hamming/hamming.js
+++ hamming/hamming.js
@@ -1,15 +1,17 @@
-var Hamming = function () {};
-  Hamming.prototype.compute = function(a, b) {
-    var count = 0;
-    if (a.length !== b.length) {
-      return 'Error: DNA strands must be of equal length.'
+var Hamming = function () {
+};
+
+Hamming.prototype.compute = function(a, b) {
+  var count = 0;
+  if (a.length !== b.length) {
+    throw new Error ('DNA strands must be of equal length.')
+  }
+  for (var i = 0; i <= a.length; i++) {
+    if (a.charAt(i) !== b.charAt(i)) {
+      count += 1;
     }
-      for (var i = 0; i <= a.length; i++) {
-        if (a.charAt(i) !== b.charAt(i)) {
-          count += 1;
-        }
-    }
-    return count;
+  }
+  return count;
 };
 
 module.exports = Hamming;
