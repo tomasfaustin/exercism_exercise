8611a4469004eec83a8e8fbb1345c53851870313
Thomas Huisking <tomasfaustin@gmail.com>
2017-07-13 16:31:04 -0700

    Take a break from this for a day

--- rna-transcription/rna-transcription.js
+++ rna-transcription/rna-transcription.js
@@ -1,8 +1,33 @@
-var Rna = function() {
-};
+// var DnaTranscriber = function() {
+// };
+//
+// DnaTranscriber.prototype.toRna = function (str) {
+//   var rna = "";
+//   for (var i = 0; i < str.length; i++) {
+//     if (str.charAt(i) === "C") {
+//       rna.push("G")
+//     }
+//   }
+//   return rna;
+// };
+//
+// module.exports = DnaTranscriber;
 
-Rna.prototype.transcribe = function () {
-
-};
-
-module.exports = Rna;
+// 
+// var translations = {
+//   A: 'U',
+//   T: 'A',
+//   C: 'G',
+//   G: 'C'
+// };
+//
+// function toRna(strand) {
+//   strand = strand.split('');
+//   return strand.map(translate).join('');
+// }
+//
+// function translate(nucleotide) {
+//   return translations[nucleotide];
+// }
+//
+// module.exports = toRna;
